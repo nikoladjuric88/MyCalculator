451df325397a4bd3f421d128c075b19343f424e7
nikoladjuric88 <djura.@gmail.com>
2017-03-08 23:06:50 +0100

    newCalcCode

--- racunaljka.js
+++ racunaljka.js
@@ -1,5 +1,6 @@
 var writes = document.getElementById('screen');
-var account = '';
+var currNumber = 0;
+var result = 0;
 
 var buttons = document.querySelector('.allButtons');
 var numberButtons = buttons.querySelectorAll('.number');
@@ -8,38 +9,14 @@ for (var i = 0; i <= numberButtons.length - 1; i++) {
 
     numberButtons[i].onclick = function() {
 
-        account += this.innerHTML;
+        var buttonNumber = parseInt(this.innerHTML);
 
-        if (account[0] === '0') {
-            account = "";
-        }
-        writes.value = account;
-        writes.focus();
-/*======================================================================================================================*/
-        var first = account.indexOf("-");
-        var second = account.indexOf("-", 1);
+        currNumber = currNumber * 10 + buttonNumber
 
-        if (account[first] === '-' && account[second] === '-') {
-            num1 = account.substring(0, second);
-            num2 = account.substring(second + 1);
-            writes.value = num2;
-        }
+        writes.value = currNumber;
 
-        var textLength = account.length;
-
-        for (var i = 0; i < textLength; i++) {
-            num1 = account.substring(0, account.indexOf('+'));
-            num2 = account.substring(account.indexOf('+') + 1);
-
-            if (account[i] === '+') {
-                writes.value = num2;
-
-                
-            }
-        }
     }
 };
-/*======================================================================================================================*/
 
 var operationButtons = buttons.querySelectorAll('.operation');
 
@@ -47,22 +24,17 @@ for (var i = 0; i <= operationButtons.length - 1; i++) {
 
     operationButtons[i].onclick = function() {
 
-        account = Number(num1) + Number(num2);
-        account += this.innerHTML;
+        var operation = this.innerHTML;
 
-        if (account[0] === '+' || account[0] === 'x' || account[0] === '/') {
-            account = "";
+        if (operation === '+') {
+
+            result += currNumber;
+
+            currNumber = 0;
+
+            writes.value = result;
+        }
 
         }
-        var first = account.indexOf("+");
-        var second = account.indexOf("+", 1);
-        num1 = account.substring(0, second);
-        num2 = account.substring(second + 1, account.lastIndexOf('+'));
-
-        if (account[first] === '+' && account[second] === '+') {
-            writes.value = account;
-        }
-        writes.focus();
-    }
-};
+ }
 
