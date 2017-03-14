d1e71274bc8a6a7cae40f1992e75232fdd70e7cb
nikola Djuric <nikola.8@gmail.com>
2017-03-14 20:47:36 +0100

    addProto

--- calculator.js
+++ calculator.js
@@ -1,60 +1,119 @@
 (function() {
 
-var writes = document.getElementById('screen');
-var currNumber = 0;
-var result = 0;
+    var inputField = document.getElementById('screen');
+    var currNumber = 0;
+    var result = 0;
 
-var buttons = document.querySelector('.allButtons');
-var numberButtons = buttons.querySelectorAll('.number');
+    var buttons = document.querySelector('.allButtons');
+    var numberButtons = buttons.querySelectorAll('.number');
+    /*=======================================================================*/
 
-for (var i = 0; i <= numberButtons.length - 1; i++) {
+    function Screen() {
 
-    numberButtons[i].onclick = function() {
+        this.element = inputField;
+    }
 
-        var buttonNumber = parseInt(this.innerHTML);
+    var screen = new Screen();
 
-        currNumber = currNumber * 10 + buttonNumber
+    Screen.prototype.setNumber = function() {
 
-        writes.value = currNumber;
+        this.element = (function() {
+
+            for (var i = 0; i <= numberButtons.length - 1; i++) {
+
+                numberButtons[i].onclick = function() {
+
+                    var buttonNumber = parseInt(this.innerHTML);
+
+                    currNumber = currNumber * 10 + buttonNumber
+
+                    inputField.value = currNumber;
+                }
+            }
+        })();
 
     }
-};
+    
+    screen.setNumber();
+    console.log(screen);
 
-var operationButtons = buttons.querySelectorAll('.operation');
+    /*=======================================================================*/
+    /*
+     Screen.prototype.setNumber = function(content) {
+            this.element = content;
 
-for (var i = 0; i <= operationButtons.length - 1; i++) {
-    var isInitialAction = true;
-    var prevOperation;
-    operationButtons[i].onclick = function() {
+         }
+       screen.setNumber(17);
+       */
+    /*=======================================================================*/
 
-        var operation = this.innerHTML;
+    /*
 
-        if (isInitialAction) {
-            result = currNumber;
-            isInitialAction = false;
+    Screen.prototype.setErrorColor = function() {
+        
+       inputField.style.backgroundColor = 'red';
+          }
+
+    screen.setErrorColor();
+
+    console.log(Screen);
+    /*
+    /*=======================================================================*/
+
+    /*
+
+      for (var i = 0; i <= numberButtons.length - 1; i++) {
+
+            numberButtons[i].onclick = function() {
+
+                var buttonNumber = parseInt(this.innerHTML);
+
+                currNumber = currNumber * 10 + buttonNumber
+
+                inputField.value = currNumber;
+
+            }
+        };
+
+    */
+
+    var operationButtons = buttons.querySelectorAll('.operation');
+
+    for (var i = 0; i <= operationButtons.length - 1; i++) {
+        var isInitialAction = true;
+        var prevOperation;
+        operationButtons[i].onclick = function() {
+
+            //    var operation = this.innerHTML; - visak 
+
+            if (isInitialAction) {
+                result = currNumber;
+                isInitialAction = false;
+            } else {
+                switch (prevOperation) {
+
+                    case '+':
+                        result = result + currNumber;
+                        break;
+
+                    case '-':
+                        result = result - currNumber;
+                        break;
+
+                    case 'x':
+                        result = result * currNumber;
+                        break;
+
+                    case '/':
+                        result = result / currNumber;
+                        break;
+                }
+
+            }
+            currNumber = 0;
+            inputField.value = result;
+            prevOperation = this.innerHTML;
         }
-        switch (prevOperation) {
-
-            case '+':
-                result = result + currNumber;
-                break;
-
-            case '-':
-                result = result - currNumber;
-                break;
-
-            case 'x':
-                result = result * currNumber;
-                break;
-
-            case '/':
-                result = result / currNumber;
-                break;
-        }
-        currNumber = 0;
-        writes.value = result;
-        prevOperation = this.innerHTML;
     }
-}
 
 })();
