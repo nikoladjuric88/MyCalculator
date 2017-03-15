57d957d8f963c4b1e89707779369d99021509f2b
nikola Djuric <nikola.8@gmail.com>
2017-03-15 20:44:02 +0100

    newCodeScreen

--- calculator.js
+++ calculator.js
@@ -6,76 +6,32 @@
 
     var buttons = document.querySelector('.allButtons');
     var numberButtons = buttons.querySelectorAll('.number');
-    /*=======================================================================*/
+
 
     function Screen() {
+        this._element = document.getElementById('screen');
+    }
 
-        this.element = inputField;
+    Screen.prototype.setNumber = function(number) {
+        this._element.value = number;
+    }
+
+    Screen.prototype.addDigit = function(digit) {
+        currNumber = currNumber * 10 + digit;
+        this.setNumber(currNumber);
     }
 
     var screen = new Screen();
 
-    Screen.prototype.setNumber = function() {
 
-        this.element = (function() {
+    for (var i = 0; i <= numberButtons.length - 1; i++) {
 
-            for (var i = 0; i <= numberButtons.length - 1; i++) {
+        numberButtons[i].onclick = function() {
 
-                numberButtons[i].onclick = function() {
-
-                    var buttonNumber = parseInt(this.innerHTML);
-
-                    currNumber = currNumber * 10 + buttonNumber
-
-                    inputField.value = currNumber;
-                }
-            }
-        })();
-
-    }
-    
-    screen.setNumber();
-    console.log(screen);
-
-    /*=======================================================================*/
-    /*
-     Screen.prototype.setNumber = function(content) {
-            this.element = content;
-
-         }
-       screen.setNumber(17);
-       */
-    /*=======================================================================*/
-
-    /*
-
-    Screen.prototype.setErrorColor = function() {
-        
-       inputField.style.backgroundColor = 'red';
-          }
-
-    screen.setErrorColor();
-
-    console.log(Screen);
-    /*
-    /*=======================================================================*/
-
-    /*
-
-      for (var i = 0; i <= numberButtons.length - 1; i++) {
-
-            numberButtons[i].onclick = function() {
-
-                var buttonNumber = parseInt(this.innerHTML);
-
-                currNumber = currNumber * 10 + buttonNumber
-
-                inputField.value = currNumber;
-
-            }
-        };
-
-    */
+            var buttonDigit = parseInt(this.innerHTML);
+            screen.addDigit(buttonDigit);
+        }
+    };
 
     var operationButtons = buttons.querySelectorAll('.operation');
 
@@ -83,8 +39,6 @@
         var isInitialAction = true;
         var prevOperation;
         operationButtons[i].onclick = function() {
-
-            //    var operation = this.innerHTML; - visak 
 
             if (isInitialAction) {
                 result = currNumber;
@@ -111,7 +65,7 @@
 
             }
             currNumber = 0;
-            inputField.value = result;
+            screen.setNumber(result);
             prevOperation = this.innerHTML;
         }
     }
