74d09d2176b0f15cf560d40c90c1d7953b0f877b
nikola Djuric <nikola.8@gmail.com>
2017-04-02 14:28:13 +0200

    add newClassScreen

--- calculator.js
+++ calculator.js
@@ -1,39 +1,9 @@
 (function() {
     "use strict";
     var result = 0;
-
+    var screen = new Screen(document.getElementById('screen'));
     var buttons = document.querySelector('.allButtons');
     var numberButtons = buttons.querySelectorAll('.number');
-
-    function Screen() {
-        this._element = document.getElementById('screen');
-        this.reset = false;
-    }
-
-    Screen.prototype.resetOnNextInput = function() {
-        this.reset = true;
-    }
-
-    Screen.prototype.setNumber = function(number) {
-        this._element.value = number;
-    }
-
-    Screen.prototype.getNumber = function() {
-        return this._element.value;
-
-    };
-
-    Screen.prototype.addDigit = function(digit) {
-
-        if (this.reset) {
-            this._element.value = digit;
-            this.reset = false;
-        } else {
-            this._element.value += digit;
-        }
-    }
-
-    var screen = new Screen();
 
     for (var i = 0; i <= numberButtons.length - 1; i++) {
 
@@ -48,10 +18,10 @@
     for (var i = 0; i <= operationButtons.length - 1; i++) {
         var isInitialAction = true;
         var prevOperation;
-        var currNumber;
 
         operationButtons[i].onclick = function() {
-            currNumber = screen.getNumber();
+            var currNumber = screen.getNumber();
+
             if (isInitialAction) {
                 result = currNumber;
                 isInitialAction = false;
@@ -76,10 +46,12 @@
                         result = Number(result) / Number(currNumber);
                         break;
                 }
+               
             }
             screen.resetOnNextInput();
-            screen.setNumber(result); 
+            screen.setNumber(result);
             prevOperation = this.innerHTML;
         }
     }
+    
 })();
