09f812416ffde7b98e6d0f0a86dd0d55750425d9
nikola Djuric <nikola.8@gmail.com>
2017-04-19 21:48:28 +0200

    5-2*2

--- racunaljka.js
+++ racunaljka.js
@@ -1,134 +1,47 @@
-(function() {
-    "use strict";
+var arrNumbers = [10, 2, 2, 2, 2];
+var arrOperations = ['/', 'x', 'x', '-'];
 
-    var callChangeProgram = require('advantageous.ops');
-    var changeProgram = new callChangeProgram();
-    var adOps = document.getElementById('advantageusOps');
-    adOps.onclick = function() {
-       changeProgram.advantageousOperationsOn(); 
-   }
+for (var i = 0; i < arrNumbers.length; i++) {
+    for (var x = 0; x < arrOperations.length; x++) {
+        for (var v = 0; v < arrOperations.length; v++) {
+            while (arrOperations[x] === '/' && i === x) {
+                arrNumbers[i] = arrNumbers[i] / arrNumbers[i + 1];
+                arrNumbers[i + 1] = 0;
+                arrNumbers.splice(arrNumbers.indexOf(0), 1);
+                arrOperations.splice(arrOperations.indexOf('/'), 1);
+            }
 
-    var x = '';
-    var result = 0;
-    var callScreen = require('calculator.output.Screen');
-    var screen = new callScreen(document.getElementById('screen'));
+            while (arrOperations[x] === 'x' && i === x) {
+                arrNumbers[i] = arrNumbers[i] * arrNumbers[i + 1];
+                arrNumbers[i + 1] = 0;
+                arrNumbers.splice(arrNumbers.indexOf(0), 1);
+                arrOperations.splice(arrOperations.indexOf('x'), 1);
 
-    var buttons = document.querySelector('.allButtons');
-    var numberButtons = buttons.querySelectorAll('.number');
- 
-    for (var i = 0; i <= numberButtons.length - 1; i++) {
-
-        numberButtons[i].onclick = function() {
-            var buttonDigit = parseInt(this.innerHTML);
-
-            if(advantageous === false) {
-            screen.addDigit(buttonDigit);
-        }
-            if(advantageous === true) {
-                //  screen.resetOnNextInput();
-                  x += buttonDigit;
-                  console.log(x);
-                  screen.addDigit(buttonDigit);
-              }
-        }
-    };
-
-    var operationButtons = buttons.querySelectorAll('.operation');
-
-    for (var i = 0; i < operationButtons.length; i++) {
-        var isInitialAction = true;
-        var prevOperation;
-
-        operationButtons[i].onclick = function() {
-            var currNumber = screen.getNumber();
-          
-            if (isInitialAction) {
-                result = currNumber;
-                isInitialAction = false;
-
-            } else {
-                switch (prevOperation) {
-                    case '+':
-                        result = Number(result) + Number(currNumber);  
-                        break;
-
-                    case '-':
-                        result = Number(result) - Number(currNumber);
-                        break;
-
-                    case 'x':
-                        result = Number(result) * Number(currNumber);
-                        break;
-
-                    case '/':
-                        result = Number(result) / Number(currNumber);
-                        break;
-                     case '=': 
-                        result = currNumber;
-                        break;
+                if (arrNumbers.length === 2 && arrOperations[x] === '/') { //ovo me zazalo ne znam sto nece / do kraja
+                    arrNumbers[i] = arrNumbers[i] / arrNumbers[i + 1];
+                    arrNumbers[i + 1] = 0;
+                    arrNumbers.splice(arrNumbers.indexOf(0), 1);
+                    arrOperations.splice(arrOperations.indexOf('/'), 1);
                 }
-
             }
-
-
-
-     if(advantageous === true) {
-       var currOps = this.innerHTML;
-
-        x += currOps;
-       console.log(x);
-      if(currOps === '=') {
-        console.log(x);
-       }
-    }
-
-
-
-
-            screen.resetOnNextInput();
-            if(advantageous === false) {
-            screen.setNumber(result);
-            }
-            if(advantageous === true) {
-                screen.setNumber(currNumber);
-            }
-            prevOperation = this.innerHTML;
-
         }
     }
-
-
-    var newNum = require('memory.num');
-    var memo = new newNum();
-
-    var numPlus = document.getElementById('memoryPlus');
-    numPlus.onclick = function() {
-        var currNumber = screen.getNumber();    
-        memo.mPlus(currNumber);
-         console.log(memo);  
-          screen.resetOnNextInput();
-     } 
-
-    var numMinus = document.getElementById('memoryMinus');
-    numMinus.onclick = function() {
-        var currNumber = screen.getNumber();
-        memo.mMinus(currNumber);
-        console.log(memo);
-        screen.resetOnNextInput();
+}
+for (var i = 0; i < arrNumbers.length; i++) {
+    for (var x = 0; x < arrOperations.length; x++) {
+        for (var v = 0; v < arrOperations.length; v++) {
+            while (arrOperations[x] === '-' && i === x) {
+                arrNumbers[i] = arrNumbers[i] - arrNumbers[i + 1];
+                arrNumbers[i + 1] = 0;
+                arrNumbers.splice(arrNumbers.indexOf(0), 1);
+                arrOperations.splice(arrOperations.indexOf('-'), 1);
+            }
+            while (arrOperations[x] === '+' && i === x) {
+                arrNumbers[i] = arrNumbers[i] + arrNumbers[i + 1];
+                arrNumbers[i + 1] = 0;
+                arrNumbers.splice(arrNumbers.indexOf(0), 1);
+                arrOperations.splice(arrOperations.indexOf('+'), 1);
+            }
+        }
     }
-
-    var mRecall = document.getElementById('memoryRecall');
-    mRecall.onclick = function() {
-       var callMemory = memo.mRecall();
-       screen.setNumber(callMemory);
-        screen.resetOnNextInput();
-    }
-
-    var mClear = document.getElementById('memoryClear');
-    mClear.onclick = function() {
-       var noMemory = memo.mClear();
-       console.log(noMemory);
-        screen.resetOnNextInput();
-    }
- 
-})();
+}
