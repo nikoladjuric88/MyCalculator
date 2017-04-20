9655e3110c6ebcd7763f1897637bfb382f403dd9
nikola Djuric <nikola.8@gmail.com>
2017-04-20 20:19:49 +0200

    opet

--- racunaljka.js
+++ racunaljka.js
@@ -1,40 +1,44 @@
-
-var arrNumbers = [10, 2, 2, 2, 3];
-var arrOperations = ['/', 'x', 'x', '+'];
+var arrNumbers = [10, 2, 2, 2, 2];
+var arrOperations = ['+', '+', 'x', 'x'];
 
 for (var i = 0; i < arrNumbers.length; i++) {
     for (var x = 0; x < arrOperations.length; x++) {
         while (arrOperations[x] === '/' && i === x) {
             arrNumbers[i] = arrNumbers[i] / arrNumbers[i + 1];
-            arrNumbers.splice(arrNumbers.indexOf(arrNumbers[i + 1]), 1);
-            arrOperations.splice(arrOperations.indexOf(arrOperations[x]), 1);
+            arrNumbers[i + 1] = 0;
+            arrNumbers.splice(arrNumbers.indexOf(0), 1);
+            arrOperations.splice(arrOperations.indexOf('/'), 1);
         }
     }
 }
 for (var i = 0; i < arrNumbers.length; i++) {
     for (var x = 0; x < arrOperations.length; x++) {
         while (arrOperations[x] === 'x' && i === x) {
-            arrNumbers[i] = arrNumbers[i] * arrNumbers[i + 1];           
-            arrNumbers.splice(arrNumbers.indexOf(arrNumbers[i + 1]), 1);
-            arrOperations.splice(arrOperations.indexOf(arrOperations[x]), 1);
+            arrNumbers[i] = arrNumbers[i] * arrNumbers[i + 1]; 
+            arrNumbers[i + 1] = 0;          
+            arrNumbers.splice(arrNumbers.indexOf(0), 1);
+            arrOperations.splice(arrOperations.indexOf('x'), 1);
         }
     }
 }
+
 for (var i = 0; i < arrNumbers.length; i++) {
     for (var x = 0; x < arrOperations.length; x++) {
         while (arrOperations[x] === '-' && i === x) {
-            arrNumbers[i] = arrNumbers[i] - arrNumbers[i + 1];           
-            arrNumbers.splice(arrNumbers.indexOf(arrNumbers[i + 1]), 1);
-            arrOperations.splice(arrOperations.indexOf(arrOperations[x]), 1);
+            arrNumbers[i] = arrNumbers[i] - arrNumbers[i + 1];  
+            arrNumbers[i + 1] = 0;           
+            arrNumbers.splice(arrNumbers.indexOf(0), 1);
+            arrOperations.splice(arrOperations.indexOf('-'), 1);
         }
     }
 }
 for (var i = 0; i < arrNumbers.length; i++) {
     for (var x = 0; x < arrOperations.length; x++) {
         while (arrOperations[x] === '+' && i === x) {
-            arrNumbers[i] = arrNumbers[i] + arrNumbers[i + 1];          
-            arrNumbers.splice(arrNumbers.indexOf(arrNumbers[i + 1]), 1);
-            arrOperations.splice(arrOperations.indexOf(arrOperations[x]), 1);
+            arrNumbers[i] = arrNumbers[i] + arrNumbers[i + 1];  
+            arrNumbers[i + 1] = 0;          
+            arrNumbers.splice(arrNumbers.indexOf(0), 1);
+            arrOperations.splice(arrOperations.indexOf('+'), 1);
         }
     }
 }
