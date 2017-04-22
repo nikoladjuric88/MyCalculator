ce9c467a2dbcd83fa2b4a98a095c79e1f67bbeee
nikola Djuric <nikola.8@gmail.com>
2017-04-22 18:02:35 +0200

    changedAdvAgain

--- advantageousOperations.js
+++ advantageousOperations.js
@@ -10,43 +10,38 @@
     Advantage.prototype.returningValue = function() {
 
         for (var i = 0; i < this.numberArr.length; i++) {
-            for (var x = 0; x < this.operationArr.length; x++) {
-                while (this.operationArr[x] === '/' && i === x) {
-                    this.numberArr[i] = Number(this.numberArr[i]) / Number(this.numberArr[i + 1]);
-                    this.numberArr[i + 1] = 0;
-                    this.numberArr.splice(this.numberArr.indexOf(0), 1);
-                    this.operationArr.splice(this.operationArr.indexOf('/'), 1);
-                }
+            if (this.operationArr[i] === '/') {
+                this.numberArr[i] = this.numberArr[i] / this.numberArr[i + 1];
+                this.numberArr[i + 1] = 0;
+                this.numberArr.splice(this.numberArr.indexOf(0), 1);
+                this.operationArr.splice(this.operationArr.indexOf('/'), 1);
             }
         }
+
         for (var i = 0; i < this.numberArr.length; i++) {
-            for (var x = 0; x < this.operationArr.length; x++) {
-                while (this.operationArr[x] === 'x' && i === x) {
-                    this.numberArr[i] = Number(this.numberArr[i]) * Number(this.numberArr[i + 1]);
-                    this.numberArr[i + 1] = 0;
-                    this.numberArr.splice(this.numberArr.indexOf(0), 1);
-                    this.operationArr.splice(this.operationArr.indexOf('x'), 1);
-                }
+            if (this.operationArr[i] === 'x') {
+                this.numberArr[i] = this.numberArr[i] * this.numberArr[i + 1];
+                this.numberArr[i + 1] = 0;
+                this.numberArr.splice(this.numberArr.indexOf(0), 1);
+                this.operationArr.splice(this.operationArr.indexOf('x'), 1);
             }
         }
+
         for (var i = 0; i < this.numberArr.length; i++) {
-            for (var x = 0; x < this.operationArr.length; x++) {
-                while (this.operationArr[x] === '-' && i === x) {
-                    this.numberArr[i] = Number(this.numberArr[i]) - Number(this.numberArr[i + 1]);
-                    this.numberArr[i + 1] = 0;
-                    this.numberArr.splice(this.numberArr.indexOf(0), 1);
-                    this.operationArr.splice(this.operationArr.indexOf('-'), 1);
-                }
+            if (this.operationArr[i] === '-') {
+                this.numberArr[i] = this.numberArr[i] - this.numberArr[i + 1];
+                this.numberArr[i + 1] = 0;
+                this.numberArr.splice(this.numberArr.indexOf(0), 1);
+                this.operationArr.splice(this.operationArr.indexOf('-'), 1);
             }
         }
+
         for (var i = 0; i < this.numberArr.length; i++) {
-            for (var x = 0; x < this.operationArr.length; x++) {
-                while (this.operationArr[x] === '+' && i === x) {
-                    this.numberArr[i] = Number(this.numberArr[i]) + Number(this.numberArr[i + 1]);
-                    this.numberArr[i + 1] = 0;
-                    this.numberArr.splice(this.numberArr.indexOf(0), 1);
-                    this.operationArr.splice(this.operationArr.indexOf('+'), 1);
-                }
+            if (this.operationArr[i] === '+') {
+                this.numberArr[i] = this.numberArr[i] + this.numberArr[i + 1];
+                this.numberArr[i + 1] = 0;
+                this.numberArr.splice(this.numberArr.indexOf(0), 1);
+                this.operationArr.splice(this.operationArr.indexOf('+'), 1);
             }
         }
         return this.numberArr;
@@ -63,7 +58,7 @@
     Advantage.prototype.addNumbers = function(number) {
         this.numberArr.push(number);
     };
-    
+
     provide('advantageous.ops', Advantage);
 
 }());
