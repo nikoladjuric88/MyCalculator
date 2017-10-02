3ce4575983aca7c0ec2e96dca469030b03fd5487
nikola Djuric <nikola.8@gmail.com>
2017-10-03 00:03:31 +0200

    changed names in binf function

--- src/eventDispatcher.js
+++ src/eventDispatcher.js
@@ -2,17 +2,17 @@ function EventDispatcher () {
 	this.eventDispatcherMap = {};
 };
 
-EventDispatcher.prototype.bind = function(nameOfFunction, whichFunction) {
-	if(this.eventDispatcherMap[nameOfFunction]) {
-		this.eventDispatcherMap[nameOfFunction].push(whichFunction);
+EventDispatcher.prototype.bind = function(eventName, handler) {
+	if(this.eventDispatcherMap[eventName]) {
+		this.eventDispatcherMap[eventName].push(handler);
 	} else {
-		this.eventDispatcherMap[nameOfFunction] = [whichFunction];
+		this.eventDispatcherMap[eventName] = [handler];
 	}
 };
 
-EventDispatcher.prototype.trigger = function(nameOfFunction) {
-	for (var i = 0; i < this.eventDispatcherMap[nameOfFunction].length; i++) {
-		this.eventDispatcherMap[nameOfFunction][i]();
+EventDispatcher.prototype.trigger = function(eventName) {
+	for (var i = 0; i < this.eventDispatcherMap[eventName].length; i++) {
+			this.eventDispatcherMap[eventName][i]();	
 	};
 };
 
