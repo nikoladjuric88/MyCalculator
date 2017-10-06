51ff74f197b3451d0f4dc3e377f838578c063022
nikola Djuric <nikola.8@gmail.com>
2017-10-06 20:23:27 +0200

    try catch

--- src/eventDispatcher.js
+++ src/eventDispatcher.js
@@ -1,26 +1,40 @@
+"use strict";
+
 function EventDispatcher() {
     this.eventDispatcherMap = {};
 };
 
+
 EventDispatcher.prototype.bind = function(eventName, handler) {
-    if (this.eventDispatcherMap[eventName]) {
-        this.eventDispatcherMap[eventName].push(handler);
-    } else {
-        this.eventDispatcherMap[eventName] = [handler];
+    if (typeof handler === 'function') {
+        if (this.eventDispatcherMap[eventName]) {
+            this.eventDispatcherMap[eventName].push(handler);
+        } else {
+            this.eventDispatcherMap[eventName] = [handler];
+        }
     }
-};
+}
 
 EventDispatcher.prototype.trigger = function(eventName) {
     if (this.eventDispatcherMap[eventName]) {
-        for (var i = 0; i < this.eventDispatcherMap[eventName].length; i++) {
-            this.eventDispatcherMap[eventName][i]();
-        };
-    };
+        try {
+            for (var i = 0; i < this.eventDispatcherMap[eventName].length; i++) {
+                this.eventDispatcherMap[eventName][i]();
+            }
+        } catch (e) {
+            this.eventDispatcherMap[eventName].splice(i, 1);
+            for (var j = 0; j < this.eventDispatcherMap[eventName].length; j++) {
+                if (j >= i) {
+                    this.eventDispatcherMap[eventName][j]();
+                }
+            }
+        }
+    }
 };
 
 EventDispatcher.prototype.unbind = function(eventName, handler) {
     if (this.eventDispatcherMap[eventName]) {
-        for (i in this.eventDispatcherMap[eventName]) {
+        for (var i = 0; i < this.eventDispatcherMap[eventName].length; i++) {
             if (this.eventDispatcherMap[eventName][i] === handler) {
                 this.eventDispatcherMap[eventName].splice(i, 1);
             }
