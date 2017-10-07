93d226e75e89641af336f91a77b28f20871a4d41
nikola Djuric <nikola.8@gmail.com>
2017-10-07 19:49:00 +0200

    error repaired

--- src/eventDispatcher.js
+++ src/eventDispatcher.js
@@ -17,16 +17,12 @@ EventDispatcher.prototype.bind = function(eventName, handler) {
 
 EventDispatcher.prototype.trigger = function(eventName) {
     if (this.eventDispatcherMap[eventName]) {
-        try {
-            for (var i = 0; i < this.eventDispatcherMap[eventName].length; i++) {
+        for (var i = 0; i < this.eventDispatcherMap[eventName].length; i++) {
+            try {
                 this.eventDispatcherMap[eventName][i]();
-            }
-        } catch (e) {
-            this.eventDispatcherMap[eventName].splice(i, 1);
-            for (var j = 0; j < this.eventDispatcherMap[eventName].length; j++) {
-                if (j >= i) {
-                    this.eventDispatcherMap[eventName][j]();
-                }
+            } catch (e) {
+                this.eventDispatcherMap[eventName].splice(i, 1);
+                i--;
             }
         }
     }
