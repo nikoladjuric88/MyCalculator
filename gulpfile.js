62585dd0a04c41d29add77ba85235622e7320de8
nikola Djuric <nikola.8@gmail.com>
2017-10-13 00:53:53 +0200

    added babelModule

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var browserify = require('gulp-browserify');
+const babel = require('gulp-babel');
 
 gulp.task('html', function() {
 	gulp.src('src/*.html')
@@ -12,8 +13,11 @@ gulp.task('css', function() {
 });
 
 gulp.task('js', function() {
-	return gulp.src('src/calculator.js')
+	return gulp.src('src/*.js')
 	.pipe(browserify({ debug: true}))
+	.pipe(babel({
+		presets: ['es2015']
+	}))
 	.pipe(gulp.dest('dist'));
 });
 
--- src/eventDispatcher.js
+++ src/eventDispatcher.js
@@ -1,44 +1,46 @@
 "use strict";
 
-function EventDispatcher() {
-    this.eventDispatcherMap = {};
-};
+class EventDispatcher {
+    constructor() {
+        this.eventDispatcherMap = {};
+    }
 
+    bind(eventName, handler) {
+        if (typeof handler === 'function') {
+            if (this.eventDispatcherMap[eventName]) {
+                this.eventDispatcherMap[eventName].push(handler);
+            } else {
+                this.eventDispatcherMap[eventName] = [handler];
+            }
+        }
+    }
 
-EventDispatcher.prototype.bind = function(eventName, handler) {
-    if (typeof handler === 'function') {
+    trigger(eventName) {
         if (this.eventDispatcherMap[eventName]) {
-            this.eventDispatcherMap[eventName].push(handler);
-        } else {
-            this.eventDispatcherMap[eventName] = [handler];
-        }
-    }
-}
+            for (var i = 0; i < this.eventDispatcherMap[eventName].length; i++) {
+                try {
+                    this.eventDispatcherMap[eventName][i]();
+                } catch (e) {
 
-EventDispatcher.prototype.trigger = function(eventName) {
-    if (this.eventDispatcherMap[eventName]) {
-        for (var i = 0; i < this.eventDispatcherMap[eventName].length; i++) {
-            try {
-                this.eventDispatcherMap[eventName][i]();
-            } catch (e) {
-
+                }
             }
         }
     }
-};
 
-EventDispatcher.prototype.unbind = function(eventName, handler) {
-    if (this.eventDispatcherMap[eventName]) {
-        for (var i = 0; i < this.eventDispatcherMap[eventName].length; i++) {
-            if (this.eventDispatcherMap[eventName][i] === handler) {
-                this.eventDispatcherMap[eventName].splice(i, 1);
+    unbind(eventName, handler) {
+        if (this.eventDispatcherMap[eventName]) {
+            for (var i = 0; i < this.eventDispatcherMap[eventName].length; i++) {
+                if (this.eventDispatcherMap[eventName][i] === handler) {
+                    this.eventDispatcherMap[eventName].splice(i, 1);
+                }
             }
         }
     }
-}
 
-EventDispatcher.prototype.unbindAll = function() {
-    this.eventDispatcherMap = {};
+    unbindAll() {
+        this.eventDispatcherMap = {};
+    }
+
 }
 
 module.exports = EventDispatcher;
