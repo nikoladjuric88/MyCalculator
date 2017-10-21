d6ee2cadb3cbfae923b73b858c587e7eeb205298
nikola Djuric <nikola.8@gmail.com>
2017-10-21 17:28:20 +0200

    addLine

--- src/precedence.js
+++ src/precedence.js
@@ -9,31 +9,31 @@ export class Precedence {
      *  Constructor.  
      */
     constructor() {
-        this.numbers = [];
-        this.operations = [];
+        this._numbers = [];
+        this._operations = [];
     }
 
     /**
      * Gives the result of precedence operations.  
      */
     calculateResult() {
-        let temporaryResult = this.numbers[0];
+        let temporaryResult = this._numbers[0];
         let finalResult = 0;
-        for (let i = 0; i < this.operations.length; i++) {
-            if (this.operations[i] === 'x') {
-                temporaryResult *= this.numbers[i + 1];
-            } else if (this.operations[i] === '/') {
-                temporaryResult /= this.numbers[i + 1];
+        for (let i = 0; i < this._operations.length; i++) {
+            if (this._operations[i] === 'x') {
+                temporaryResult *= this._numbers[i + 1];
+            } else if (this._operations[i] === '/') {
+                temporaryResult /= this._numbers[i + 1];
             } else {
-                let sign = this.operations[i] === '+' ? 1 : -1;
-                if (i !== this.operations.length - 1 && this.operations[i + 1] === '/' || this.operations[i + 1] === 'x') {
+                let sign = this._operations[i] === '+' ? 1 : -1;
+                if (i !== this._operations.length - 1 && this._operations[i + 1] === '/' || this._operations[i + 1] === 'x') {
                     finalResult += temporaryResult;
-                    temporaryResult = sign * this.numbers[i + 1];
+                    temporaryResult = sign * this._numbers[i + 1];
                 } else {
-                    temporaryResult += sign * this.numbers[i + 1];
+                    temporaryResult += sign * this._numbers[i + 1];
                 }
             }
-            if (i === this.operations.length - 1) {
+            if (i === this._operations.length - 1) {
                 finalResult += temporaryResult;
             }
         }
@@ -41,18 +41,11 @@ export class Precedence {
     }
 
     /**
-     * Allows calculation with precedence operations.  
-     */
-    precedenceOn() {
-        this.turnOn = true;
-    }
-
-    /**
      * Adds a operation. 
      * @param {String}  operation. 
      */
     addOperation(operation) {
-        this.operations.push(operation);
+        this._operations.push(operation);
     }
 
     /**
@@ -60,6 +53,6 @@ export class Precedence {
      * @param {Number}  number. 
      */
     addNumber(number) {
-        this.numbers.push(number);
+        this._numbers.push(number);
     }
 }
