ff0bbd67031f98837e2db14facc1d7bcf3e459c5
nikola Djuric <nikola.8@gmail.com>
2017-11-06 19:05:21 +0100

    added jsDoc

--- src/digitKeyboard.js
+++ src/digitKeyboard.js
@@ -1,10 +1,21 @@
 import { EventDispatcher } from './eventDispatcher.js';
 
+/**
+ *  DigitKeyboard sets the digits on the screen.
+ */
 export class DigitKeyboard extends EventDispatcher {
+
+    /**
+     *  Constructor.  
+     */
     constructor() {
         super();
         this.bindEvents();
     }
+
+    /**
+     * Sets entered number on the screen.
+     */
     bindEvents() {
         let buttons = document.querySelector('.allButtons');
         let numberButtons = buttons.querySelectorAll('.number');
@@ -12,7 +23,7 @@ export class DigitKeyboard extends EventDispatcher {
         for (let i = 0; i < numberButtons.length; i++) {
             let self = this;
             numberButtons[i].onclick = function() {
-                let buttonDigit = this.innerHTML;
+                let buttonDigit = Number(this.innerHTML);
                 self.trigger('DigitKeyboard.KEY_PRESSED', buttonDigit);
             }
         };
