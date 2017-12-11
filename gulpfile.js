d01ee50f3b1ca3dab3a002ebb501418cce8e5420
nikola Djuric <nikola.8@gmail.com>
2017-12-11 09:13:37 +0100

    added concat in json

--- gulpfile.js
+++ gulpfile.js
@@ -3,32 +3,34 @@ const browserify = require('gulp-browserify');
 const babel = require('gulp-babel');
 const babelify = require('babelify');
 const sass = require('gulp-sass');
+const concat = require('gulp-concat');
 
 gulp.task('html', function() {
-	gulp.src('src/*.html')
-	    .pipe(gulp.dest('dist'));
+    gulp.src('src/*.html')
+        .pipe(gulp.dest('dist'));
 });
 
 gulp.task('sass', function() {
-	return gulp.src('src/style.scss')
-	.pipe(sass({ sourceComments: 'map' }))
-	.pipe(gulp.dest('dist'))
+    return gulp.src('src/**/*.scss')
+        .pipe(sass())
+        .pipe(concat('style.css')) 
+        .pipe(gulp.dest('dist'));
 });
 
 gulp.task('js', function() {
     return gulp.src('src/*.js')
-    .pipe(browserify({ debug: true}))
-    .pipe(babel({
-        presets: ['es2015'],
-        sourceType: module
-    }))
-    .pipe(gulp.dest('dist'));
+        .pipe(browserify({ debug: true }))
+        .pipe(babel({
+            presets: ['es2015'],
+            sourceType: module
+        }))
+        .pipe(gulp.dest('dist'));
 });
 
 gulp.task('watch', function() {
-	gulp.watch('src/**/*.html', ['html']);
-	gulp.watch('src/style.scss', ['sass']);
-	gulp.watch('src/**/*.js', ['js']);
+    gulp.watch('src/**/*.html', ['html']);
+    gulp.watch('src/**/*.scss', ['sass']);
+    gulp.watch('src/**/*.js', ['js']);
 });
 
 gulp.task('default', ['html', 'js', 'watch', 'sass']);
